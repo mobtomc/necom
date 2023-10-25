3de78c3d735730829e33432225355b09a59c4b75
aryan bhat <aryabhat99@gmail.com>
2023-10-25 20:17:10 +0530

    minio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {BrowserRouter as Router,Route,Routes} from  'react-router-dom';
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return <div className='overflow-hidden'>react app</div>;
